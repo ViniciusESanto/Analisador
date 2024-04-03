c482c1a6c1f494ee7ebde40703f7800c590c4243
Vinicius E Santo <viniciusesant@gmail.com>
2024-04-03 13:16:41 -0300

    Finalização de Projeto
    Finalização do projeto.
    -Mudanças de linha no arquivo .js e .html

--- main.js
+++ main.js
@@ -29,5 +29,32 @@ function adicionar() {
     }else {
         window.alert('Valor invalido ou ja encontrado na lista')
     }
+    num.value = ``
+    num.focus()
+}
 
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de Finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores [pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor informado é ${maior}.</p>`
+        res.innerHTML += `<p> O menor valor informado é ${menor}.</p>`
+        res.innerHTML += `<p> Somando temos o valor ${soma}.</p>`
+        res.innerHTML += `<p> A media dos valores é ${media}.</p>`
+    }
 }
